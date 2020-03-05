5ae0570f9c6313187c95c06406b73338c90905f3
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-03-05 12:25:10 +0900

    [Finisu]2-8

---
+++ その他/elsif-else.rb
@@ -0,0 +1,18 @@
+apple = "Aomori"
+
+if apple == "Nagano"
+	puts "リンゴの生産地は長野です"
+else
+	puts "リンゴの生産地は青森です"
+end
+
+
+apple = "Yamagata"
+
+if apple == "Yamagata"
+	puts "リンゴの生産地は山形です"
+elsif apple =="Nagano"
+	puts "リンゴの生産地は長野です"
+else
+	puts "リンゴの生産地は青森です"
+end
